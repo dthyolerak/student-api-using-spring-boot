d03031367182e78bcca4db1f7111e7b325ce0734
dthyolerak <dthyolerak1.com@mail.com>
2022-01-01 17:08:15 +0200

    testing by displaying hello world on browser unima-lb website

--- src/main/java/com/example/unimalb/UnimaLbApplication.java
+++ src/main/java/com/example/unimalb/UnimaLbApplication.java
@@ -2,12 +2,18 @@ package com.example.unimalb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UnimaLbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnimaLbApplication.class, args);
 	}
-
+		@GetMapping
+		public String hello(){
+		return "Hello there";
+		}
 }
