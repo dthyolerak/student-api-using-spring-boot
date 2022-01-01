1cf3d97ea0f3a777e0c848f66a24f40333d0dc0b
dthyolerak <dthyolerak1.com@mail.com>
2022-01-01 16:46:29 +0200

    initial commint unima-lb website

---
+++ src/main/java/com/example/unimalb/UnimaLbApplication.java
@@ -0,0 +1,13 @@
+package com.example.unimalb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimaLbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimaLbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unimalb/UnimaLbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unimalb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimaLbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
